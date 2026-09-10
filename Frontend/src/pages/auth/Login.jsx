900de7584934984397caa9dd26abc1b3360869ad
pawan-sahu-2027 <pawan.sahu.2027>
2026-09-11 01:00:44 +0530

    error message

--- Frontend/src/pages/auth/Login.jsx
+++ Frontend/src/pages/auth/Login.jsx
@@ -36,6 +36,7 @@ console.log("Sending:", userDetails);
     } catch (error) {
   console.log("Status:", error.response?.status);
   console.log("Data:", error.response?.data);
+         alert(error.response?.data);
   console.log(error);
 }
   };
